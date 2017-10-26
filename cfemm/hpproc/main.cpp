20d24f48e2c18ddd519b9f82ea9ee0b862516fb9
Dmitry Ponyatov <dponyatov@gmail.com>
2017-10-26 13:03:50 +0400

    + fork from sf/mercurial
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cfemm/hpproc/main.cpp
@@ -0,0 +1,123 @@
+/*
+   This code is a modified version of an algorithm
+   forming part of the software program Finite
+   Element Method Magnetics (FEMM), authored by
+   David Meeker. The original software code is
+   subject to the Aladdin Free Public Licence
+   version 8, November 18, 1999. For more information
+   on FEMM see www.femm.info. This modified version
+   is not endorsed in any way by the original
+   authors of FEMM.
+
+   This software has been modified to use the C++
+   standard template libraries and remove all Microsoft (TM)
+   MFC dependent code to allow easier reuse across
+   multiple operating system platforms.
+
+   Date Modified: 2014 - 03 - 21
+   By:  Emoke Szelitzky
+        Tibor Szelitzky
+        Richard Crozier
+   Contact:
+        szelitzkye@gmail.com
+        sztibi82@gmail.com
+        richard.crozier@yahoo.co.uk
+*/
+
+#include <iostream>
+#include <cstdio>
+#include "hpproc.h"
+#include "femmcomplex.h"
+
+using namespace std;
+
+
+int main()
+{
+    string mystr;
+    HPProc testHPProc;
+    CComplex out;
+    CPointVals u;
+    double x, y;
+
+    cout << "HPProc Loaded!" << endl;
+
+
+    int test = testHPProc.OpenDocument("./test/Temp0.anh");
+
+    if (test==true)
+    {
+        for(int i=0; i<(int)(testHPProc.blocklist.size()); i++)
+        {
+            if (testHPProc.blocklist[i].IsSelected == true)
+            {
+                testHPProc.blocklist[i].IsSelected = false;
+            }
+        }
+
+       // for(int j=0; j<(int)(testHPProc.blocklist.size()); j++)
+        {
+          //  if (testHPProc.blocklist[j].InGroup==3)
+            {
+                testHPProc.blocklist[0].ToggleSelect();
+            }
+
+         //   testHPProc.bHasMask = false;
+        }
+
+
+        out = testHPProc.BlockIntegral(0);
+        printf ("Block Temperature Integral for block 0 %f\n", out.Re());
+
+        out = testHPProc.BlockIntegral(1);
+        printf ("Block Cross-section Area Integral for block 0 %f\n", out.Re());
+
+        out = testHPProc.BlockIntegral(2);
+        printf ("Block Volume Integral for block 0 %f\n", out.Re());
+
+        out = testHPProc.BlockIntegral(3);
+        printf ("Block Average F Integral for block 0 Fx: %f, Fy: %f\n", out.Re(), out.Im());
+
+        out = testHPProc.BlockIntegral(4);
+        printf ("Block Average G Integral for block 0 Gx: %f, Gy: %f\n", out.Re(), out.Im());
+
+        testHPProc.Smooth = false;
+
+        printf ("Field Smoothing OFF\n");
+
+        // get point values
+        x = 0.01;
+        y = 0.01;
+        testHPProc.GetPointValues(x, y, u);
+        printf ("Point vals at x = %f, y = %f\nT: %f\tFx: %f\tFy: %f\tKx: %f\tKy: %f\tGx: %f\tGy: %f\n",
+                x, y, u.T, u.F.Re(), u.F.Im(), u.K.Re(), u.K.Im(), u.G.Re(), u.G.Im());
+
+        // get point values
+        x = 0.005;
+        y = 0.005;
+        testHPProc.GetPointValues(x, y, u);
+        printf ("Point vals at x = %f, y = %f\nT: %f\tFx: %f\tFy: %f\tKx: %f\tKy: %f\tGx: %f\tGy: %f\n",
+                x, y, u.T, u.F.Re(), u.F.Im(), u.K.Re(), u.K.Im(), u.G.Re(), u.G.Im());
+
+        testHPProc.Smooth = true;
+
+        printf ("Field Smoothing ON\n");
+
+        // get point values
+        x = 0.01;
+        y = 0.01;
+        testHPProc.GetPointValues(x, y, u);
+        printf ("Point vals at x = %f, y = %f\nT: %f\tFx: %f\tFy: %f\tKx: %f\tKy: %f\tGx: %f\tGy: %f\n",
+                x, y, u.T, u.F.Re(), u.F.Im(), u.K.Re(), u.K.Im(), u.G.Re(), u.G.Im());
+
+        // get point values
+        x = 0.005;
+        y = 0.005;
+        testHPProc.GetPointValues(x, y, u);
+        printf ("Point vals at x = %f, y = %f\nT: %f\tFx: %f\tFy: %f\tKx: %f\tKy: %f\tGx: %f\tGy: %f\n",
+                x, y, u.T, u.F.Re(), u.F.Im(), u.K.Re(), u.K.Im(), u.G.Re(), u.G.Im());
+
+    }
+
+    return 0;
+}
---
+++ release.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#
+## file: release.sh
+#
+# This script create releases of xfemm for various platforms. Windows builds require
+# MXE: http://mxe.cc/
+#
+# Run this script from the directory containing it
+
+# stop on first error
+set -e
+
+VERSION="1_9"
+WORKING_COPY_DIR=$(pwd)
+echo $WORKING_COPY_DIR
+mkdir $WORKING_COPY_DIR/release
+
+# create release directory
+
+# linux 64 bit
+#mkdir $WORKING_COPY_DIR/release/xfemm_linux64
+LINUX_64_DIR="xfemm_linux64"
+LINUX_64_RELEASE_DIR="$WORKING_COPY_DIR/release/$LINUX_64_DIR"
+# export from the working directory to the release directory
+hg archive $LINUX_64_RELEASE_DIR
+# remove file created by mercurial
+rm $LINUX_64_RELEASE_DIR/.hg_archival.txt
+# remove the release script
+rm $LINUX_64_RELEASE_DIR/release.sh
+rm $LINUX_64_RELEASE_DIR/test_release.sh
+# create temp build directory
+rm -rf /tmp/$LINUX_64_DIR
+mkdir -p /tmp/$LINUX_64_DIR
+# run cmake from temp dir and build
+cd /tmp/$LINUX_64_DIR
+cmake -DCMAKE_BUILD_TYPE=Release $LINUX_64_RELEASE_DIR/cfemm
+make
+
+if ! [ -x "$(command -v matlab)" ]; then
+  echo 'matlab is not installed, not building mex files using Matlab.' >&2
+else
+  # buld the mex files using matlab
+  matlab -nodesktop -r "restoredefaultpath; cd('$LINUX_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
+  #matlab -nodesktop -r "restoredefaultpath; cd('$LINUX_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', false, 'ForceMexRecompile', true, 'CrossBuildW64', true); quit"
+fi
+
+# tar up the result in the release directory
+cd $WORKING_COPY_DIR/release
+tar cvzf xfemm_v${VERSION}_linux64.tar.gz $LINUX_64_DIR/
+
+
+# repeat the above, but cross-compiling for windows, requires MXE: http://mxe.cc/
+#
+
+# win 32 -- i686-pc-mingw32
+#svn export $WORKING_COPY_DIR $WORKING_COPY_DIR/release/xfemm_mingw_win32 --native-eol CRLF
+#rm $WORKING_COPY_DIR/release/xfemm_mingw_win32/release.sh
+WIN_64_DIR="xfemm_mingw_win64"
+WIN_64_RELEASE_DIR="$WORKING_COPY_DIR/release/$WIN_64_DIR"
+# win 64 -- x86_64-w64-mingw32 MXE target
+hg archive $WIN_64_RELEASE_DIR # TODO: windows line endings?
+# remove file created by hg
+rm $WIN_64_RELEASE_DIR/.hg_archival.txt
+# remove release scripts
+rm $WIN_64_RELEASE_DIR/release.sh
+rm $WIN_64_RELEASE_DIR/test_release.sh
+# create temp build directory
+rm -rf /tmp/$WIN_64_DIR
+mkdir -p /tmp/$WIN_64_DIR
+# run cmake from temp dir and build cfemm
+cd /tmp/$WIN_64_DIR
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DEXTRA_CMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" \
+      -DEXTRA_CMAKE_EXE_LINKER_FLAGS="-static  -static-libgcc -static-libstdc++" \
+      -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/x86_64-w64-mingw32.static/share/cmake/mxe-conf.cmake $WIN_64_RELEASE_DIR/cfemm
+make
+
+# copy over the windows dlls, and delete from dist
+#cp $WORKING_COPY_DIR/release/xfemm_mingw_win64/windeps/* $WORKING_COPY_DIR/release/xfemm_mingw_win64/cfemm/lib/
+#cp $WORKING_COPY_DIR/release/xfemm_mingw_win64/windeps/* $WORKING_COPY_DIR/release/xfemm_mingw_win64/cfemm/bin/
+#rm -r $WORKING_COPY_DIR/release/xfemm_mingw_win64/windeps/
+
+if ! [ -x "$(command -v matlab)" ]; then
+  echo 'matlab is not installed, not building mex files using Matlab.' >&2
+else
+  # buld the mex files using matlab
+  matlab -nodesktop -r "restoredefaultpath; cd('$WIN_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', true, 'ForceMexRecompile', true); quit"
+  #matlab -nodesktop -r "restoredefaultpath; cd('$WIN_64_RELEASE_DIR/mfemm'); mfemm_setup('Runtests', false, 'ForceMexRecompile', true, 'CrossBuildW64', true); quit"
+fi
+
+# zip up the result in the release directory
+#cd $WORKING_COPY_DIR/release
+#zip -r xfemm_v${VERSION}_mingw_win64.zip $WIN_64_DIR/
+
+# test
+cd $WORKING_COPY_DIR
+./test_release.sh
+
